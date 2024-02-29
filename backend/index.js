1451bf584e8f93ce3c14c423f33379d6b603705c
bhanu chandra <80154731+bhanuylm75@users.noreply.github.com>
2024-02-29 17:35:54 +0530

    fixed some

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,18 @@ const xyz="production"
 
 const __dirname1 = path.resolve();
 
+if (xyz === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
 const serverr=app.listen(3008,()=>{
   console.log("server started")
 })
